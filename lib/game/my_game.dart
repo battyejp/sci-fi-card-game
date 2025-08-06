2067651e7cbd7e82314fdb9807a97793bae67a95
John Battye <John.Battye@ipfin.co.uk>
2025-08-06 16:58:43 +0100

    Remove unused import and demo method from MyGame class for cleaner code

--- lib/game/my_game.dart
+++ lib/game/my_game.dart
@@ -1,7 +1,6 @@
 import 'package:flame/game.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'components/card_deck.dart';
 import 'data/game_constants.dart';
 
@@ -32,16 +31,6 @@ class MyGame extends FlameGame with HasGameReference {
   // Method to update card count in hand
   void updateHandSize(int newCardCount) {
     cardDeck.updateCardCount(newCardCount);
-  }
-  
-  // Demo method to cycle through different hand sizes
-  void demoHandSizes() {
-    // This could be called periodically to demonstrate the fanned layout
-    final handSizes = [3, 5, 7, 4, 6, 5]; // Different hand sizes to demo
-    int currentIndex = 0;
-    
-    // You could implement a timer or trigger to cycle through these
-    // For now, this serves as documentation of the capability
   }
   
   // Getter for card deck
